fa8ca8bf0f446031d038512fde2e2d4e44e92e6b
Orville Dillon <60524259+eLEPHANTCase@users.noreply.github.com>
2021-02-15 22:09:42 -0500

    Fix: Fixed the count of random numbers from 65 to 69 to match Powerball!

--- src/main.py
+++ src/main.py
@@ -8,3 +8,10 @@ Here's a lottery number generator... Try it. Feeling lucky!?
 rnum = 0
 vlist = []
 
+while len(vlist) < 5:
+    rnum = random.randint(0,70)
+    if rnum not in vlist:
+        vlist.append(rnum)
+
+print('The potential winning lottery numbers are:\n')
+print(vlist[:], ":", random.randint(0,25))
